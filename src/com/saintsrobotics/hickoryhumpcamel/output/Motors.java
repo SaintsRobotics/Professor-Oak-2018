580893b19fccc6fed7ce2eb404956a0fb17d9a28
SAIO <THISISASHARED@lLAPTOP.WHatev>
2018-01-27 13:28:02 -0800

    Made Motors and everything work probaby

--- src/com/saintsrobotics/hickoryhumpcamel/output/Motors.java
+++ src/com/saintsrobotics/hickoryhumpcamel/output/Motors.java
@@ -1,20 +1,43 @@
 package com.saintsrobotics.hickoryhumpcamel.output;
 
 import java.util.HashMap;
-import java.util.Map;
+import java.util.LinkedList;
+import java.util.List;
+import java.lang.reflect.*;
 import com.github.dozer.output.*;
 
 public abstract class Motors {
-  private Map<String, MotorRamping> motorList = new HashMap<>();
-
+  private LinkedList<MotorRamping> rampedMotors;
+  private LinkedList<Motor> motors;
+  public Motors() {
+	  int length = this.getClass().getDeclaredFields().length;
+	  this.motors = new LinkedList<>();
+	  try {
+		for(Field f : this.getClass().getDeclaredFields()) {
+		  Object obj = f.get(this);
+		  if(obj instanceof Motor) {
+			this.motors.addLast((Motor)obj);
+			if(obj instanceof MotorRamping) {
+			  this.rampedMotors.addLast((MotorRamping) obj);
+			}
+		  }
+		}
+	  } catch (IllegalArgumentException e) {
+			//PUT SOME CODE THAT KILLS THE ROBOT HERE
+			e.printStackTrace();
+	  } catch (IllegalAccessException e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
+	  }
+  }
+  
+  public abstract void init();
+  
   public void stopAll() {
-    motorList.values().forEach(MotorRamping::stop);
+    for(Motor motor : this.motors) motor.stop();
   }
 
   public void update() {
-    motorList.values().forEach(MotorRamping::update);
-  }
-  public Motor get(String motorName) {
-	  return this.motorList.get(motorName);
+	  for(MotorRamping motor : this.rampedMotors) motor.update();
   }
 }
