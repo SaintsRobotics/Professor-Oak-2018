dc4142123dcd825b573e8aaef67202a10370a721
Keval Shah <captainrex493@gmail.com>
2018-01-31 15:51:11 -0800

    Lift Task with motors getting a value from smart dashboard to hold the lifting mechanism up.

--- src/com/saintsrobotics/hickoryhumpcamel/tasks/teleop/LiftTaskWithMotor.java
+++ src/com/saintsrobotics/hickoryhumpcamel/tasks/teleop/LiftTaskWithMotor.java
@@ -1,16 +1,20 @@
 package com.saintsrobotics.hickoryhumpcamel.tasks.teleop;
 
 import com.saintsrobotics.hickoryhumpcamel.Robot;
+
+import edu.wpi.first.wpilibj.smartdashboard.SmartDashboard;
+
 import com.github.dozer.coroutine.helpers.RunEachFrameTask;
 
 
 public class LiftTaskWithMotor extends RunEachFrameTask {
   
-    private static double holdPosition = 0.11; //Add actual value later
+    private static double holdPosition; //Add actual value later
     
     
     @Override
     protected void runEachFrame() {
+    	holdPosition = SmartDashboard.getNumber("holdPosition", 0.11);
         double MovementAmount = Robot.instance.oi.xboxInput.rightTrigger() - Robot.instance.oi.xboxInput.leftTrigger();
         
         if (MovementAmount != 0) {
@@ -19,8 +23,7 @@ public class LiftTaskWithMotor extends RunEachFrameTask {
         else {
         	Robot.instance.motors.lifter.set(holdPosition);
         }
-    	
-    
+    	 
     }
         
         
