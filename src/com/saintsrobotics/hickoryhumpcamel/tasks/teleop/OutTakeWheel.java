7f3d94e4a3bc332a4c893fd4a0961f40656beceb
Keval Shah <captainrex493@gmail.com>
2018-02-06 12:36:37 -0800

    Added way to push out cube for 3 seconds when Left bumper is pressed.
    Change time limit to match what is necessary.

--- src/com/saintsrobotics/hickoryhumpcamel/tasks/teleop/OutTakeWheel.java
+++ src/com/saintsrobotics/hickoryhumpcamel/tasks/teleop/OutTakeWheel.java
@@ -5,12 +5,18 @@ import com.github.dozer.coroutine.helpers.RunEachFrameTask;
 
 
 public class OutTakeWheel extends RunEachFrameTask {
+	private long time = 0;
+	private long startTime;
     @Override
     protected void runEachFrame() {
         boolean trig = Robot.instance.oi.xboxInput.LB();
         
-        while(trig) {
-        	Robot.instance.motors.intake.set(-0.1);
+        if(trig) {
+        	startTime = System.currentTimeMillis();
+        	while (time < 3000) { //Adjust time limit as needed
+        		Robot.instance.motors.intake.set(-0.1);
+        		this.time = System.currentTimeMillis() - startTime;
+        	}
         	
         }
     }
