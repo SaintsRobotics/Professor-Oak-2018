dde7a7c9e28d11f9d7e4e9baebe7dcb73cecb8b2
Drew Thompson <horsenkat@gmail.com>
2018-03-02 16:12:19 -0800

    finished most auton cases and set up taskChooser
    auton cases finished: s1e1, s1e4, s2e1, s2e4, s3e1, s3e4 
    wasn't sure how best to do taskChooser so emulated CoreBot-2017

--- src/com/saintsrobotics/hickoryhumpcamel/tasks/auton/choose/LeftSwitchAuton.java
+++ src/com/saintsrobotics/hickoryhumpcamel/tasks/auton/choose/LeftSwitchAuton.java
@@ -2,21 +2,57 @@ package com.saintsrobotics.hickoryhumpcamel.tasks.auton.choose;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.function.Supplier;
 
 import com.github.dozer.coroutine.Task;
 import com.github.dozer.coroutine.helpers.RunSequentialTask;
 import com.saintsrobotics.hickoryhumpcamel.Robot;
-import com.saintsrobotics.hickoryhumpcamel.tasks.auton.AutonLiftTask;
 import com.saintsrobotics.hickoryhumpcamel.tasks.auton.ForwardAtHeadingTask;
-import com.saintsrobotics.hickoryhumpcamel.tasks.auton.SameSideSwitchAuton;
 import com.saintsrobotics.hickoryhumpcamel.tasks.auton.TurnToHeadingTask;
 import com.saintsrobotics.hickoryhumpcamel.util.ForwardConfiguration;
-import com.saintsrobotics.hickoryhumpcamel.util.LiftConfiguration;
 import com.saintsrobotics.hickoryhumpcamel.util.TurnConfiguration;
 
-public class LeftSwitchAuton extends RunSequentialTask {
-	public LeftSwitchAuton(ForwardConfiguration pidConfig) {
-		super(Robot.instance.switchStatus ? new SameSideSwitchAuton(): new OppositeSideSwitchAuton());
+
+
+public class LeftSwitchAuton extends RunSequentialTask implements Supplier<Task> {
+	static List<Task> taskList = new ArrayList<Task>(); 
+	public LeftSwitchAuton() {
+		super(taskChoose());
+	}
+
+	private static Task[] taskChoose() { 
+		List<Task> taskList = new ArrayList<Task>(); 
+		if(Robot.instance.switchStatus) {
+			taskList.add(new ForwardAtHeadingTask(0, 2, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(-45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 2.82, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 6.18, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 1.161, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 1.23, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+		}
+		else { 
+			taskList.add(new ForwardAtHeadingTask(0, 2, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 2.58, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 16.6, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(-45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 2.98, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 3, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(-45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 2.52, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+			taskList.add(new ForwardAtHeadingTask(0, 3.29, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+		}
+		return taskList.toArray(new Task[taskList.size()]);
+	}
+
+	@Override
+	public Task get() {
+		return this; 
 	}
 }
-
--- src/com/saintsrobotics/hickoryhumpcamel/tasks/auton/choose/RightSwitchAuton.java
+++ src/com/saintsrobotics/hickoryhumpcamel/tasks/auton/choose/RightSwitchAuton.java
@@ -1,33 +1,51 @@
 package com.saintsrobotics.hickoryhumpcamel.tasks.auton.choose;
 
+
+	import java.util.ArrayList;
+import java.util.List;
+
+import com.github.dozer.coroutine.Task;
 import com.github.dozer.coroutine.helpers.RunSequentialTask;
 import com.saintsrobotics.hickoryhumpcamel.Robot;
 import com.saintsrobotics.hickoryhumpcamel.tasks.auton.ForwardAtHeadingTask;
-import com.saintsrobotics.hickoryhumpcamel.tasks.auton.SameSideSwitchAuton;
 import com.saintsrobotics.hickoryhumpcamel.tasks.auton.TurnToHeadingTask;
 import com.saintsrobotics.hickoryhumpcamel.util.ForwardConfiguration;
 import com.saintsrobotics.hickoryhumpcamel.util.TurnConfiguration;
 
-public class RightSwitchAuton {
-	public RightSwitchAuton() {
-		super(Robot.instance.switchStatus ? new OppositeSideSwitchAuton(-1): new SameSideSwitchAuton(-1));
+	public class RightSwitchAuton extends RunSequentialTask {
+		public RightSwitchAuton() {
+			super(taskChoose());
+		}
 
+		private static Task[] taskChoose() { 
+			List<Task> taskList = new ArrayList<Task>(); 
+			if(Robot.instance.switchStatus) {
+				taskList.add(new TurnToHeadingTask(-45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 5.2, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 13.7, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0,  3.8, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 2.8, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 2.15, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 1.9, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+
+			}
+			else { 
+				taskList.add(new ForwardAtHeadingTask(0, 2.6, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 3.6, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 4, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(-45, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 2.2, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+				taskList.add(new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)));
+				taskList.add(new ForwardAtHeadingTask(0, 1.6, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)));
+			}
+			return taskList.toArray(new Task[taskList.size()]);
+		}
 	}
 
-	
-	
-	new RunSequentialTask(
-			new TurnToHeadingTask(-45, new TurnConfiguration(Robot.instance.sensors.gyro)),
-			new ForwardAtHeadingTask(0, 5.2, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)),
-			new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)),
-			new ForwardAtHeadingTask(0, 13.7, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)),
-			new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)),
-			new ForwardAtHeadingTask(0, 3.8, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)),
-			new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)),
-			new ForwardAtHeadingTask(0, 2.8, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)),
-			new TurnToHeadingTask(45, new TurnConfiguration(Robot.instance.sensors.gyro)),
-			new ForwardAtHeadingTask(0, 2.15, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)),
-			new TurnToHeadingTask(0, new TurnConfiguration(Robot.instance.sensors.gyro)),
-			new ForwardAtHeadingTask(0, 1.9, new ForwardConfiguration(Robot.instance.sensors.gyro, Robot.instance.sensors.average)),
-}
-}
